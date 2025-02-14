9d0fe71b0559c35c283ffc29b93f04dae12e7218
PrymushAnton <anton.primush123663@gmail.com>
2025-02-14 21:40:26 +0200

    Function for displaying images was created

--- modules/gui/app_image.py
+++ modules/gui/app_image.py
@@ -1,9 +1,27 @@
+"""
+У цьому файлі створили клас для відображення зображень
+
+"""
+
 import PIL.Image
 import customtkinter as ctk
 import PIL
 class Image_label(ctk.CTkLabel):
-    def __init__(self, ch_master, image_path, **kwargs):
+    def __init__(self, ch_master, image_path, **kwargs):      
+        """
+            Цей клас використовуєтся для створення зображень
+            
+            Нижче вказані параметри конструктору класу
+            - `ch_master` - параметр який відповідає за те, на чому буде намальовано зображення
+            - `image_path` - параметр відповідаючий за шлях до картинки
+            
+            Властивості класу:
+            - `self.image_path` - ця влстивість відповідає за шлях до картинки
+        """   
+
+
         self.image_path = image_path
+        
         ctk.CTkLabel.__init__(
             self,
             master=ch_master,
@@ -11,7 +29,12 @@ class Image_label(ctk.CTkLabel):
             image=self.image_load(),
             **kwargs
         )
-    def image_load(self):
+        
+    def image_load(self): 
+        """
+            Цей метод використовується для завантаження зображення
+        """
+
         try:
             pillow_image = PIL.Image.open(fp = self.image_path)
             image = ctk.CTkImage(
@@ -22,3 +45,4 @@ class Image_label(ctk.CTkLabel):
         except Exception as exception:
             print(exception)
             return None
+
--- modules/tools/search_image.py
+++ modules/tools/search_image.py
@@ -1,11 +1,27 @@
 """
     Цей файл відповідіє за відкриття діалогового вікна з вибором файлів
 """
+import customtkinter as ctk
 from ..gui.app_button import App_button
 # module filedialog - з бібліотеки tkinter, дозволяє працювати з діалоговим вікном
 from tkinter import filedialog
 from ..gui.app_image import Image_label
 
+image_list = []
+
+def show_image(frame: ctk.CTkFrame, name_image:str):
+    print(name_image)
+    # frame.winfo_children() - повертає список всіх дочерніх віджетів, які знаходяться у frame
+    children = frame.winfo_children()
+    for child in children:
+        # isinstance(object, class) - перевіряє, чи є object екземпляром класу class. Повертає True або False
+        if isinstance(child, Image_label):
+            # widget.pack_forget() - приховує widget, який було розміщено за допомогою pack()
+            child.pack_forget()
+    for image in image_list:
+        if image.image_path.split("/")[-1] == name_image:
+            image.pack()
+            
 # askopenfiles - відкриває діалогове вікно, і повертає кортеж з відкритими обраними файлами
 # askopenfilenames - відкриває діалогове вікно, і повертає кортеж з повними шляхами обраних файлів
 
@@ -20,16 +36,22 @@ def image_search(parent, ch_master, dashboard):
         filetypes=[("Image files",["*.png", "*.jpg", "*.jpeg"])],
         parent = parent
     )
+    
     # split - розбиває рядок на елементи списку по символу, який ми вказали
     for i in filepath:
         #! print(i.split("/")[-1])
+        
         button = App_button(
             size=50,
             ch_master=ch_master,
             fg_color="#181818",
             hover_color="#373535",
-            text=i.split("/")[-1] 
+            text=i.split("/")[-1],
+            command = lambda name=i.split("/")[-1]: show_image(frame=dashboard, name_image=name)
         )
+
+        
+        
         
         # pack - розміщує об'єкти по черзі.
         # підлаштовує розміри батьківського фрейму під розміри його вмісту
@@ -38,5 +60,6 @@ def image_search(parent, ch_master, dashboard):
             ch_master= dashboard,
             image_path = i
         )
-        picture.pack()
+        image_list.append(picture)
+        # picture.pack()
     
